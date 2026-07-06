14881a0504475683ae0b73ad44c6177a8005ad5e
naman <namanpandey3120@gmail.com>
2026-07-07 01:24:43 +0530

    Prod:Deployed Frontend and Backend

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export default async function ConnectToMGDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("successful connection to DB")
   } catch (err) {
     console.error(`Connection To MGDB Failed ${err}`);
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRouter from "./Features/Auth/auth.route.js";
 
 ConnectToMGDB();
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -13,7 +14,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:process.env.VITE_FRONTEND_URL ,
     credentials: true,
   }),
 );
@@ -24,6 +25,6 @@ app.get("/", (req, res) => {
   res.send("hello user");
 });
 
-app.listen(3000, () => {
-  console.log(`server is happy on http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`SERVER IS ACTIVE ON ${PORT}`);
 });
