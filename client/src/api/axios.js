535e7b246c783ee6879016cf7dcade06f7897021
naman <namanpandey3120@gmail.com>
2026-07-11 03:26:12 +0530

    Fix: Added explicit headers on axios global declaration

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,6 +5,9 @@ const api = axios.create({
   
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  }
 });
 
 export default api;
